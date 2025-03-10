f7900708418edee55c9f66fd04ee756ca9ea9ddb
Magical1426 <156017269+Magical1426@users.noreply.github.com>
2025-03-10 20:55:58 +0000

    Added comments for Lab partner

--- EE273Project/Deconvolution.cpp
+++ EE273Project/Deconvolution.cpp
@@ -30,7 +30,7 @@ void Deconvolution::setDeconvolution(int stride, int padding, int kernel_size) {
     this->Kernel_size = kernel_size;
 }
 
-void Deconvolution::setKernel(MatrixXd kernel) {
+void Deconvolution::setKernel(MatrixXd kernel) { //checks whether or not the matrix has the correct dimensions
     if (kernel.rows() == this->Kernel_size && kernel.cols() == this->Kernel_size) {
         this->kernel = kernel;
     }
@@ -39,22 +39,21 @@ void Deconvolution::setKernel(MatrixXd kernel) {
     }
 }
 
-
 MatrixXd Deconvolution::start(MatrixXd input) {
     int strideDimensions = this->Stride * (input.rows() - 1) + this->Kernel_size - 2 * this->Padding;
-    int paddedDimensions = strideDimensions + 2 * this->Padding;
+	int paddedDimensions = strideDimensions + 2 * this->Padding; //variable that stores the dimensions of the padded and strided matrix
 
 
-    MatrixXd Output = MatrixXd::Zero(paddedDimensions, paddedDimensions);
+    MatrixXd Output = MatrixXd::Zero(paddedDimensions, paddedDimensions); //storing the results at the end 
 
-    for (int i = 0; i < input.rows(); i++) {
+	for (int i = 0; i < input.rows(); i++) { //this iterates through the input matrix
         for (int j = 0; j < input.cols(); j++) {
             Output.block(i * this->Stride, j * this->Stride, this->Kernel_size, this->Kernel_size) +=
-                input(i, j) * this->kernel;
+                input(i, j) * this->kernel; //updating the output 
         }
     }
 
-    cout << "Input Size: " << input.rows() << "x" << input.cols() << endl;
+    cout << "Input Size: " << input.rows() << "x" << input.cols() << endl; //debugg
     cout << "Kernel Size: " << this->Kernel_size << endl;
     cout << "Stride: " << this->Stride << ", Padding: " << this->Padding << endl;
     cout << "Output Size: " << Output.rows() << "x" << Output.cols() << endl;
